abb0e8cfe09c9eb230d832c378bb1b85eaa50364
Clodoaldo <clodoaldosantosmatos@hotmail.com>
2025-02-03 21:54:35 -0400

    feature: adicionado um calculo de bonus

--- src/academy/devdojo/semanaum/Lclassesabstratas/dominio/Funcionario.java
+++ src/academy/devdojo/semanaum/Lclassesabstratas/dominio/Funcionario.java
@@ -7,6 +7,10 @@ public abstract class Funcionario {
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calcularDesconto();
+    }
+    public double calcularDesconto (){
+        return this.salario = this.salario + this.salario *0.1;
     }
 
 }
